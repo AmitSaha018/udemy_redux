cd9eca6982bcd3905ff29f32f1d470713e390966
AmitSaha018 <31251262+AmitSaha018@users.noreply.github.com>
2022-03-16 21:00:11 +0530

    Using a slice for authentication and using it where its required.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Fragment } from 'react';
 import Counter from './components/Counter';
+import Headers from './components/Header';
+import Auth from './components/Auth';
+import { useSelector } from 'react-redux';
+import UserProfile from './components/UserProfile';
 
 
 function App() {
+  const auth = useSelector(state => state.auth.isAuthenticated);
   return (
-    <Counter />
+    <Fragment>
+      <Headers />
+      {!auth && <Auth/>}
+      {auth && <UserProfile/>}
+      <Counter />
+    </Fragment>
   );
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,48 @@
-
-
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
 const counterSilce = createSlice({
-    name: 'counter',
-    initialState,
-    reducers: {
-        increment(state) {
-            state.counter++;
-            // we can directly mutate the state in redux toolkit
-            // because redux toolkit uses a package named emmer
-            // under the hood to not mutate the state directly.
-         },
-        decrement(state) {
-            state.counter--;
-         },
-        increase(state, action) {
-            state.counter = state.counter + action.payload;
-         },
-        toggleCounter(state) {
-            state.showCounter = !state.showCounter;
-         }
-    }
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+      // we can directly mutate the state in redux toolkit
+      // because redux toolkit uses a package named emmer
+      // under the hood to not mutate the state directly.
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const initialAuthState = { isAuthenticated: false };
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+    login(state) {
+      state.isAuthenticated = true;
+    },
+  },
 });
 
 const store = configureStore({
-    reducer: counterSilce.reducer
+  reducer: { counter: counterSilce.reducer, auth: authSlice.reducer },
 });
-export const counterActions = counterSilce.actions
+export const counterActions = counterSilce.actions;
+export const authActions = authSlice.actions;
 
 export default store;
